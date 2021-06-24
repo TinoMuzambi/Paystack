e1b0c79143401f7bdd4ae228eb3eac9ef6028dcf
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-24 22:54:16 +0200

    Updates env.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	env: {
-		PAYSTACK_PUBLIC_KEY: process.env.PAYSTACK_PUBLIC_KEY,
+		PAYSTACK_PUBLIC_TEST_KEY: process.env.PAYSTACK_PUBLIC_TEST_KEY,
+		PAYSTACK_SECRET_TEST_KEY: process.env.PAYSTACK_SECRET_TEST_KEY,
 	},
 };
