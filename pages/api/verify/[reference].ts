90cf82e6fbe00c7dd4b62385d094ee1ebbde20a5
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-24 22:41:32 +0200

    Fixes destructuring.

--- pages/api/verify/[reference].ts
+++ pages/api/verify/[reference].ts
@@ -1,10 +1,37 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import https from "https";
 
 type Data = {
 	name: string;
 };
 
 export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-	res.status(200).json({ name: "John Doe" });
+	const {
+		query: { ref },
+		method,
+	} = req;
+
+	// const options = {
+	// 	hostname: "api.paystack.co",
+	// 	port: 443,
+	// 	path: "/transaction/verify/:reference",
+	// 	method: "GET",
+	// 	headers: {
+	// 		Authorization: "Bearer SECRET_KEY",
+	// 	},
+	// };
+	// https
+	// 	.request(options, (res) => {
+	// 		let data = "";
+	// 		res.on("data", (chunk) => {
+	// 			data += chunk;
+	// 		});
+	// 		res.on("end", () => {
+	// 			console.log(JSON.parse(data));
+	// 		});
+	// 	})
+	// 	.on("error", (error) => {
+	// 		console.error(error);
+	// 	});
+	res.status(200).json({ name: ref as string });
 };
