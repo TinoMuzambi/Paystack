56479d6b64dd4a25d490b6e9cd71662a9f558b3c
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-24 22:32:40 +0200

    Adds types.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
 import { usePaystackPayment } from "react-paystack";
+import { PaystackProps } from "react-paystack/dist/types";
 
 export default function Home() {
-	const config = {
-		reference: new Date().getTime(),
+	const config: PaystackProps = {
+		reference: new Date().getTime().toLocaleString(),
 		email: "user@example.com",
+		label: "Tino",
 		amount: 20000,
-		publicKey: process.env.PAYSTACK_PUBLIC_KEY,
+		publicKey: process.env.PAYSTACK_PUBLIC_KEY as string,
 		currency: "ZAR",
 	};
 
 	// you can call this function anything
-	const onSuccess = (reference) => {
+	const onSuccess: Function = (reference: string) => {
 		// Implementation for whatever you want to do with reference and after success call.
 		console.log(reference);
 	};
 
 	// you can call this function anything
-	const onClose = () => {
+	const onClose: Function = () => {
 		// implementation for  whatever you want to do when the Paystack dialog closed.
 		console.log("closed");
 	};
