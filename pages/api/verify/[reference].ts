76b91013760266f9cee2d29dc34011cb2354cb20
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-25 03:13:25 +0200

    Cleaning up api route.

--- pages/api/verify/[reference].ts
+++ pages/api/verify/[reference].ts
@@ -11,15 +11,6 @@ export default async (req: NextApiRequest, resp: NextApiResponse<Data>) => {
 		query: { reference },
 	} = req;
 
-	const options = {
-		hostname: "api.paystack.co",
-		port: 443,
-		path: `/transaction/verify/${reference}`,
-		method: "GET",
-		headers: {
-			Authorization: `Bearer ${process.env.PAYSTACK_SECRET_TEST_KEY}`,
-		},
-	};
 	try {
 		const res = await fetch(
 			`https://api.paystack.co/transaction/verify/${reference}`,
@@ -33,7 +24,6 @@ export default async (req: NextApiRequest, resp: NextApiResponse<Data>) => {
 		const data = await res.json();
 		resp.status(200).json({ success: true, data: data.data });
 	} catch (error) {
-		console.error("oh nooo", error);
 		resp.status(400).json({ success: false });
 	}
 };
