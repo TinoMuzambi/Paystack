17de53caa602495277f29864d0ffbf50eefc56ab
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-25 02:12:02 +0200

    Adds form.

--- pages/index.tsx
+++ pages/index.tsx
@@ -50,7 +50,28 @@ export default function Home() {
 	const PaystackHookExample = () => {
 		const initializePayment = usePaystackPayment(config);
 		return (
-			<div>
+			<main>
+				<form id="paymentForm">
+					<div className="form-group">
+						<label htmlFor="email">Email Address</label>
+						<input type="email" id="email-address" required />
+					</div>
+					<div className="form-group">
+						<label htmlFor="amount">Amount</label>
+						<input type="tel" id="amount" required />
+					</div>
+					<div className="form-group">
+						<label htmlFor="first-name">First Name</label>
+						<input type="text" id="first-name" />
+					</div>
+					<div className="form-group">
+						<label htmlFor="last-name">Last Name</label>
+						<input type="text" id="last-name" />
+					</div>
+					<div className="form-submit">
+						<button type="submit"> Pay </button>
+					</div>
+				</form>
 				<button
 					onClick={() => {
 						initializePayment(onSuccess, onClose);
@@ -58,7 +79,7 @@ export default function Home() {
 				>
 					Paystack Hooks Implementation
 				</button>
-			</div>
+			</main>
 		);
 	};
 
