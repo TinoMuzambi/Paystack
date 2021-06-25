021805a2dcaf7bf5c3650b585add3bb547072793
Tino Muzambi <tinomuzambi@gmail.com>
2021-06-25 03:18:16 +0200

    Adds head.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,23 @@
+import Head from "next/head";
+
 import Paystack from "../components/Paystack";
 
 const Home: React.FC = (): JSX.Element => {
 	return (
-		<main>
-			<Paystack />
-		</main>
+		<>
+			<Head>
+				<title>Paystack Playground</title>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin="true"
+				/>
+			</Head>
+			<main>
+				<Paystack />
+			</main>
+		</>
 	);
 };
 
