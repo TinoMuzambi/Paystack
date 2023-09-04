8cca501319a49fb3c3390f44411c71e8bb16b569
Tinotenda Muzambi <tinomuzambi@gmail.com>
2023-09-04 08:20:48 +0200

    Adds integration.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 import "../sass/App.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Script
+				src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3820040354624651"
+				crossOrigin="anonymous"
+			/>
+		</>
+	);
 }
 export default MyApp;
