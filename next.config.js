fa8824a7c7228852c60cce6ad368583db9376ffa
Tino <tinotenda@ovex.io>
2025-08-21 23:35:40 +0200

    Another fix.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     PAYSTACK_PUBLIC_TEST_KEY: process.env.PAYSTACK_PUBLIC_TEST_KEY,
     PAYSTACK_SECRET_TEST_KEY: process.env.PAYSTACK_SECRET_TEST_KEY,
+    NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+    NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
   },
-  skipTrailingSlashRedirect: true,
 };
