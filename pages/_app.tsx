3fb6fe898d18e2eca2d975b6ca211a3a8615e59a
Tino <tinotenda@ovex.io>
2025-08-22 00:44:04 +0200

    Revert "Goes back to instrumentatino client."
    This reverts commit e1e02cab3ec016013752fd53d929d60d5276db55.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,27 @@
 import type { AppProps } from "next/app";
 
 import "../sass/App.scss";
+import { useEffect } from "react";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: "https://paystack-blue.vercel.app/ingest",
+      defaults: "2025-05-24",
+      // Enable debug mode in development
+      loaded: (posthog) => {
+        if (process.env.NODE_ENV === "development") posthog.debug();
+      },
+    });
+  }, []);
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />;
+    </PostHogProvider>
+  );
 }
 
 export default MyApp;
